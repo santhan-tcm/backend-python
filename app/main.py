b342d360042bc39b493898ea0144657736f90b87
santhan <santhan.v@tickingminds.com>
2026-02-13 17:25:31 +0530

    added frontend url

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(title="ISO 20022 Validation API")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*","https://front-end-iso.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
